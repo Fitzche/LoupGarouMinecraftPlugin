f8b9760ca53de606f920087707dbe8bacde83d12
Fitzche <fitz3.ytb@gmail.com>
2025-02-04 22:39:30 +0100

    save

--- src/WorldEditUtil/StructureLoader.java
+++ src/WorldEditUtil/StructureLoader.java
@@ -1,6 +1,7 @@
 package WorldEditUtil;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -9,6 +10,7 @@ import org.bukkit.Location;
 
 import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.world.registry.WorldData;
 
 import fr.fitzche.lgmore.Main;
@@ -22,8 +24,10 @@ import com.sk89q.worldedit.event.extent.EditSessionEvent;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
 import com.sk89q.worldedit.extent.clipboard.io.SchematicWriter;
+import com.sk89q.worldedit.function.operation.Operation;
 
 public class StructureLoader {
 	
@@ -36,7 +40,7 @@ public class StructureLoader {
 			writer = ClipboardFormat.SCHEMATIC.getWriter(new FileOutputStream(saveFile));
 			try {
 				EditSession editSession = new EditSession(BukkitUtil.getLocalWorld(Main.world), 1000);
-				wow
+				
 				writer.write(clip, editSession.getWorld().getWorldData());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -51,8 +55,14 @@ public class StructureLoader {
 		
 	}
 
-	public static void load(Location primary, Location secondary, File sourceFile) {
+	public static Clipboard load(Location primary, Location secondary, File sourceFile) {
+		try {
+			erreur
 		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//ClipboardFormats.findByFile(sourceFile).getReader(new FileInputStream(sourceFile));
 	}
 	public static BlockVector locToVector(Location loc) {
 		
